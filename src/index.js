8435623b6302a68e24ce7d807f62736b1a44b80f
Anri <anri0806@hotmail.co.jp>
2021-09-23 16:53:32 -0400

    done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("#create-task-form")
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      submitForm(e.target.new_task_description.value, e.target.due_date.value);
+  
+      form.reset()
+    });
 });
+
+//submit form
+function submitForm(todo, date) {
+  let li = document.createElement("li");
+  let p = document.createElement("p")
+  let btn = document.createElement("button");
+  
+  btn.addEventListener("click", deleteTask)
+
+  li.innerText = `${todo} `;
+  p.innerText = `due by ${date}`
+  li.appendChild(p)
+  btn.innerText = "x";
+  li.appendChild(btn);
+  document.querySelector("#tasks").appendChild(li);
+}
+
+//delete form
+function deleteTask(e) {
+  e.target.parentNode.remove();
+}
+
